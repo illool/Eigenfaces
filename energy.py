c9dcb98b446fdab727d0058e2ad50e4a2a55f2bc
illool <32729865+illool@users.noreply.github.com>
2018-05-11 16:05:54 +0800

    Update energy.py

--- energy.py
+++ energy.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python
-
-__author__ = 'Aleksandar Gyorev'
-__email__  = 'a.gyorev@jacobs-university.de'
+# coding=utf-8
+# modify for run on windows with python3.5
+__author__ = 'Aleksandar Gyorev and modify by illool@163.com'
+__email__ = 'a.gyorev@jacobs-university.de'
 
 import os
 import cv2
@@ -19,15 +19,19 @@ gnuplot script (./plot_energy.gpi).
 """
 if __name__ == '__main__':
     if len(sys.argv) < 2 or len(sys.argv) > 3:
-        print 'Usage: python2.7 energy.py <att faces dir>'
-        sys.exit(1)
-
-    energy_values = range(5,101)[0:101:5]                                       # we consider all energies multiples of 5
-    #energy_values = [85 85 85 85 85]                                            # run 5 times with 85% energy
+        print('Usage: python3.5 energy.py <att faces dir>')
+        path = r'F:\PythonPro\Eigenfaces\att_faces'
+        # sys.exit(1)
+    else:
+        path = str(sys.argv[1])
+    # we consider all energies multiples of 5
+    energy_values = range(5, 101)[0:101:5]
+    # energy_values = [85 85 85 85 85]
+    # # run 5 times with 85% energy
 
     f = open('energy.dat', 'w')
     for energy_value in energy_values:
-        efaces = EF.Eigenfaces(str(sys.argv[1]), float(energy_value / 100.0))
+        efaces = EF.Eigenfaces(path, float(energy_value / 100.0))
         efaces.evaluate()
         f.write('%d %.6lf\n' % (energy_value, efaces.accuracy))
 
